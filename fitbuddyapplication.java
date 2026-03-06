7c86eb279a0283a9c9b1de19e4c09e7a49e73430
kaushalkachhway <kaushalkachhway@gmail.com>
2026-03-06 03:16:28 -1200

    Add files via upload

---
+++ fitbuddyapplication.java
@@ -0,0 +1,9 @@
+package com.fitbuddy;
+
+public class FitBuddyApplication {
+
+    public static void main(String[] args) {
+        System.out.println("FitBuddy AI Fitness Plan Generator Started");
+    }
+
+}
